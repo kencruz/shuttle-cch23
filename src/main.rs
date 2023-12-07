8b30dcb2759c61cfa9fc27637536495f8c87bb17
Ken Cruz <ken@kencruz.ca>
2023-12-06 21:14:56 -0500

    shuttle cch day 01

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use axum::{http::StatusCode, routing::get, Router};
+use axum::{extract::Path, http::StatusCode, routing::get, Router};
+use std::collections::HashMap;
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
@@ -8,11 +9,30 @@ async fn make_error() -> StatusCode {
     StatusCode::INTERNAL_SERVER_ERROR
 }
 
+async fn exclusive_cube(Path(params): Path<HashMap<String, String>>) -> String {
+    let mut nums = params
+        .get("nums")
+        .unwrap()
+        .split("/")
+        .map(|n| n.parse::<u32>().unwrap())
+        .into_iter();
+
+    let first = nums.next().unwrap();
+    let xor = nums.fold(first, |n, acc| n ^ acc);
+    let cubed = xor.pow(3);
+
+    format!("{:?}\n", cubed)
+}
+
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
+    let day_minus_one = Router::new().route("/error", get(make_error));
+    let day_one = Router::new().route("/*nums", get(exclusive_cube));
+
     let router = Router::new()
         .route("/", get(hello_world))
-        .route("/-1/error", get(make_error));
+        .nest("/-1", day_minus_one)
+        .nest("/1", day_one);
 
     Ok(router.into())
 }
