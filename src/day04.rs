7b1fb042179a9250e910a757668e94937b3babee
Ken Cruz <ken@kencruz.ca>
2023-12-08 22:11:46 -0500

    shuttle cch day 04 - task 2

--- src/day04.rs
+++ src/day04.rs
@@ -1,13 +1,98 @@
 use axum::{extract, Json};
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize)]
 pub struct Reindeer {
     name: String,
     strength: u32,
+    speed: Option<f32>,
+    height: Option<u32>,
+    antler_width: Option<u32>,
+    snow_magic_power: Option<u32>,
+    favorite_food: Option<String>,
+    #[serde(rename = "cAnD13s_3ATeN-yesT3rdAy")]
+    candies_eaten_yesterday: Option<u32>,
+}
+
+impl Clone for Reindeer {
+    fn clone(&self) -> Reindeer {
+        Reindeer {
+            name: self.name.clone(),
+            strength: self.strength,
+            speed: self.speed,
+            height: self.height,
+            antler_width: self.antler_width,
+            snow_magic_power: self.snow_magic_power,
+            favorite_food: self.favorite_food.clone(),
+            candies_eaten_yesterday: self.candies_eaten_yesterday,
+        }
+    }
+}
+
+#[derive(Serialize)]
+pub struct ContestResponse {
+    fastest: String,
+    tallest: String,
+    magician: String,
+    consumer: String,
 }
 
 pub async fn strength(extract::Json(payload): extract::Json<Vec<Reindeer>>) -> String {
     let sum = payload.into_iter().fold(0, |sum, r| r.strength + sum);
     format!("{}", sum)
+}
+
+pub async fn contest(
+    extract::Json(payload): extract::Json<Vec<Reindeer>>,
+) -> Json<ContestResponse> {
+    let mut fastest: Option<Reindeer> = None;
+    let mut tallest: Option<Reindeer> = None;
+    let mut magician: Option<Reindeer> = None;
+    let mut consumer: Option<Reindeer> = None;
+
+    payload.into_iter().for_each(|r| {
+        if fastest.is_none() || &fastest.as_ref().unwrap().speed.unwrap() < &r.speed.unwrap() {
+            fastest = Some(r.clone());
+        }
+
+        if tallest.is_none() || &tallest.as_ref().unwrap().height.unwrap() < &r.height.unwrap() {
+            tallest = Some(r.clone());
+        }
+
+        if magician.is_none()
+            || &magician.as_ref().unwrap().snow_magic_power.unwrap() < &r.snow_magic_power.unwrap()
+        {
+            magician = Some(r.clone());
+        }
+
+        if consumer.is_none()
+            || &consumer.as_ref().unwrap().candies_eaten_yesterday.unwrap()
+                < &r.candies_eaten_yesterday.unwrap()
+        {
+            consumer = Some(r.clone());
+        }
+    });
+
+    Json(ContestResponse {
+        fastest: format!(
+            "Speeding past the finish line with a strength of {} is {}",
+            fastest.clone().unwrap().strength,
+            fastest.unwrap().name
+        ),
+        tallest: format!(
+            "{} is standing tall with his {} cm wide antlers",
+            tallest.clone().unwrap().name,
+            tallest.unwrap().antler_width.unwrap(),
+        ),
+        magician: format!(
+            "{} could blast you away with a snow magic power of {}",
+            magician.clone().unwrap().name,
+            magician.unwrap().snow_magic_power.unwrap(),
+        ),
+        consumer: format!(
+            "{} ate lots of candies, but also some {}",
+            consumer.clone().unwrap().name,
+            consumer.unwrap().favorite_food.unwrap()
+        ),
+    })
 }
--- src/router.rs
+++ src/router.rs
@@ -1,8 +1,8 @@
 use axum::routing::post;
-use axum::{extract::Path, routing::get, Router};
+use axum::{routing::get, Router};
 
 use crate::day01::exclusive_cube;
-use crate::day04::strength;
+use crate::day04::{contest, strength};
 use crate::day_minus_one::make_error;
 
 async fn hello_world() -> &'static str {
@@ -12,7 +12,9 @@ async fn hello_world() -> &'static str {
 pub fn create_api_router() -> Router {
     let day_minus_one = Router::new().route("/error", get(make_error));
     let day_one = Router::new().route("/*nums", get(exclusive_cube));
-    let day_four = Router::new().route("/strength", post(strength));
+    let day_four = Router::new()
+        .route("/strength", post(strength))
+        .route("/contest", post(contest));
 
     Router::new()
         .route("/", get(hello_world))
